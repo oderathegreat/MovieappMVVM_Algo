1bbd7c06909a574bf79a44c07a84f2db36cde1b2
oderathegreat <oderathegreat@gmail.com>
2022-11-11 10:43:57 +0300

    model to hold our movie detailsm

--- app/src/main/java/com/example/movieappalgo/model/Movie.kt
+++ app/src/main/java/com/example/movieappalgo/model/Movie.kt
@@ -1,7 +1,11 @@
 package com.example.movieappalgo.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Movie(
-
-
-
+   val title: String,
+   @SerializedName("release_date")
+   val releaseDate: String,
+   @SerializedName("poster_path")
+   val poster_Path:String
 )
---
+++ app/src/main/java/com/example/movieappalgo/model/Movie_Detail.kt
@@ -0,0 +1,24 @@
+package com.example.movieappalgo.model
+
+import android.media.Rating
+import com.google.gson.annotations.SerializedName
+
+data class Movie_Detail(
+
+
+    val overview:String,
+    val revenue: String,
+    val popularity: String,
+    val title:String,
+    val rating: Double,
+    val video : Boolean,
+    @SerializedName("poster_path")
+    val poster_Path:String,
+    val id:Int,
+    @SerializedName("release_date")
+    val releaseDate:String
+
+
+
+
+)

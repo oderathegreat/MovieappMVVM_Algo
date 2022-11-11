a3f455cb04d041f8650a6cb405e630c57405576a
oderathegreat <oderathegreat@gmail.com>
2022-11-11 10:48:45 +0300

    Response model to hold data

--- app/src/main/java/com/example/movieappalgo/apiservice/Movie_Interface.kt
+++ app/src/main/java/com/example/movieappalgo/apiservice/Movie_Interface.kt
@@ -1,5 +1,6 @@
 package com.example.movieappalgo.apiservice
 
+import com.example.movieappalgo.model._Response
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -9,7 +10,7 @@ interface Movie_Interface {
 
 
     @GET("movie/popular")
-    fun getPopular_Movie(@Query("page")page:Int) : Single<MovieResponse>
+    fun getPopular_Movie(@Query("page")page:Int) : Single<_Response>
     @GET("movie/{movie_id}")
-    fun getMovie_Detail(@Path("movie_id")id:Int) : Single<MovieDetail>
+    fun getMovie_Detail(@Path("movie_id")id:Int) : Single<_Response>
 }
---
+++ app/src/main/java/com/example/movieappalgo/model/_Response.kt
@@ -0,0 +1,9 @@
+package com.example.movieappalgo.model
+
+data class _Response(
+
+val _totalPageCount:Int,
+val _totalResults:Int,
+val movie_List: List<Movie>
+
+)

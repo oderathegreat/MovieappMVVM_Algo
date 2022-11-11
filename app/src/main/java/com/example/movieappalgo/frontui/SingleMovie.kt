61a10942c98f9fa3989deb8117a1043778a6a47e
oderathegreat <oderathegreat@gmail.com>
2022-11-11 17:23:15 +0300

    single movie view model to hold data for single movied

--- app/src/main/java/com/example/movieappalgo/frontui/SingleMovie.kt
+++ app/src/main/java/com/example/movieappalgo/frontui/SingleMovie.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 import com.example.movieappalgo.R
 
 class SingleMovie : AppCompatActivity() {
+
+//    private lateinit var viewModel: SingleMovieViewModel
+//    private lateinit var movieRepository: MovieDetailesRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_single)

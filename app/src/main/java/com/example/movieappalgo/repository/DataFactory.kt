290196565468a253960bb3ca16794df9af15cbec
oderathegreat <oderathegreat@gmail.com>
2022-11-11 12:01:36 +0300

    data factory view model implementationl

--- app/src/main/java/com/example/movieappalgo/repository/DataFactory.kt
+++ app/src/main/java/com/example/movieappalgo/repository/DataFactory.kt
@@ -1,6 +1,23 @@
 package com.example.movieappalgo.repository
 
-class DataFactory {
+import androidx.lifecycle.MutableLiveData
+import androidx.paging.DataSource
+import com.example.movieappalgo.apiservice.Movie_Interface
+import com.example.movieappalgo.model.Movie
+import io.reactivex.disposables.CompositeDisposable
+
+class DataFactory (private val api : Movie_Interface, private val compdisposable: CompositeDisposable)
+    : DataSource.Factory<Int, Movie >() {
+
+    val _moviesliveData =  MutableLiveData<_DataMovieSource>()
 
 
-}
+
+    override fun create(): DataSource<Int, Movie> {
+        val movieDataSource = _DataMovieSource(api,compdisposable)
+
+       _moviesliveData.postValue(movieDataSource)
+        return movieDataSource
+    }
+
+    }

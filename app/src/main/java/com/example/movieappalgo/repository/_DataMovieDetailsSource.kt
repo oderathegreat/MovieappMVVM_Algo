396f4b746b2edd65bf7dbd0941061fc91bef667a
oderathegreat <oderathegreat@gmail.com>
2022-11-11 12:15:24 +0300

    movie details view model network state initilization

--- app/src/main/java/com/example/movieappalgo/repository/_DataMovieDetailsSource.kt
+++ app/src/main/java/com/example/movieappalgo/repository/_DataMovieDetailsSource.kt
@@ -1,5 +1,16 @@
 package com.example.movieappalgo.repository
 
-class _DataMovieDetailsSource {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.movieappalgo.apiservice.Movie_Interface
+import io.reactivex.disposables.CompositeDisposable
+
+class _DataMovieDetailsSource(private val api: Movie_Interface, private val composedispose: CompositeDisposable) {
+
+    private val network_State = MutableLiveData<Network_State>()
+
+    val networkState: LiveData<Network_State>
+        get() = network_State
+
 
 }

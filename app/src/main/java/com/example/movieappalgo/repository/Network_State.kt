0a330d7b3f0a9f5bb9411c954fd22b6ccedbe218
oderathegreat <oderathegreat@gmail.com>
2022-11-11 11:26:11 +0300

    defining our network state

--- app/src/main/java/com/example/movieappalgo/repository/Network_State.kt
+++ app/src/main/java/com/example/movieappalgo/repository/Network_State.kt
@@ -1,4 +1,38 @@
 package com.example.movieappalgo.repository
 
-class Network_State {
+
+enum class Status {
+    RUNNING,
+    SUCCESS,
+    FAILED
+
+}
+
+
+class Network_State(val status: Status, val msg: String) {
+
+    companion object {
+
+        val ENDOFLIST:Network_State
+        val ERROR: Network_State
+        val LOADED: Network_State
+        val LOADING: Network_State
+
+        init {
+
+            ERROR = Network_State(Status.FAILED, "Opps Something happened hold on")
+
+            ENDOFLIST = Network_State(Status.FAILED, "Limit reached! Sorry")
+
+            LOADED = Network_State(Status.SUCCESS, "Hurray! Success")
+
+            LOADING = Network_State(Status.RUNNING, "Relax! We are currently getting some data")
+
+
+        }
+
+
+    }
+
+
 }
--- app/src/main/java/com/example/movieappalgo/repository/_DataMovieSource.kt
+++ app/src/main/java/com/example/movieappalgo/repository/_DataMovieSource.kt
@@ -1,11 +1,13 @@
 package com.example.movieappalgo.repository
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.paging.PageKeyedDataSource
 import com.example.movieappalgo.apiservice.INITIAL_PAGE
 import com.example.movieappalgo.apiservice.Movie_Interface
 import com.example.movieappalgo.model.Movie
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
 class _DataMovieSource (private val api_Service: Movie_Interface, private val disposable: CompositeDisposable) :
     PageKeyedDataSource<Int, Movie>() {
@@ -13,5 +15,25 @@ class _DataMovieSource (private val api_Service: Movie_Interface, private val di
     private var page = INITIAL_PAGE
 
     val networkState: MutableLiveData<Network_State> = MutableLiveData()
+
+    override fun loadInitial(params: LoadInitialParams<Int>, callback: LoadInitialCallback<Int, Movie>) {
+
+        networkState.postValue(Network_State.LOADING)
+
+        disposable.add(
+            api_Service.getPopular_Movie(page)
+                .subscribeOn(Schedulers.io())
+                .subscribe(
+                    {
+                        callback.onResult(it.movie_List, null, page+1)
+                        networkState.postValue(Network_State.LOADED)
+                    },
+                    {
+                        networkState.postValue(Network_State.ERROR)
+                        it.message?.let { it1 -> Log.e("MovieDataSource", it1) }
+                    }
+                )
+        )
+    }
 
 }

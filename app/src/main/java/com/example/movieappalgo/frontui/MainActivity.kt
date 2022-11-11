0efab0d0691663c260b4e3fe2ab1f51c8bc23aaf
oderathegreat <oderathegreat@gmail.com>
2022-11-11 12:37:58 +0300

    ui frontend package folder

--- app/src/main/java/com/example/movieappalgo/frontui/MainActivity.kt
+++ app/src/main/java/com/example/movieappalgo/frontui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.movieappalgo
+package com.example.movieappalgo.frontui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.movieappalgo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/movieappalgo/repository/_DataMovieDetailsSource.kt
+++ app/src/main/java/com/example/movieappalgo/repository/_DataMovieDetailsSource.kt
@@ -1,9 +1,13 @@
 package com.example.movieappalgo.repository
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.movieappalgo.apiservice.Movie_Interface
+import com.example.movieappalgo.model.Movie_Detail
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import java.lang.Exception
 
 class _DataMovieDetailsSource(private val api: Movie_Interface, private val composedispose: CompositeDisposable) {
 
@@ -11,6 +15,36 @@ class _DataMovieDetailsSource(private val api: Movie_Interface, private val comp
 
     val networkState: LiveData<Network_State>
         get() = network_State
+
+    private val _downloadedMovieDetailsResponse = MutableLiveData<Movie_Detail>()
+    val downloadedMovieResponse: LiveData<Movie_Detail>
+        get() = _downloadedMovieDetailsResponse
+
+    fun fetchMovieDetails(movieId: Int) {
+        network_State.postValue(Network_State.LOADING)
+        try {
+            composedispose.add(
+                api.getMovie_Detail(movieId)
+                    .subscribeOn(Schedulers.io())
+                    .subscribe(
+                        {
+                            _downloadedMovieDetailsResponse
+                            network_State.postValue(Network_State.LOADED)
+                        },
+                        {
+                            network_State.postValue(Network_State.ERROR)
+                            it.message?.let { it1 -> Log.e("MovieDetailsDataSource", it1) }
+                        }
+                    )
+            )
+
+        }
+
+        catch (e: Exception){
+            e.message?.let { Log.e("MovieDetailsDataSource", it) }
+        }
+
+    }
 
 
 }
